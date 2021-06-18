3186f784b00b9cec5c431c6b997f6a1875cfd63a
Ming Zhu <zhumphy@gmail.com>
2021-06-17 20:50:30 -0400

    format files

--- worker/index.js
+++ worker/index.js
@@ -1,12 +1,12 @@
 const keys = require("./keys");
 const redis = require("redis");
 
-const redisClinet = redis.createClient({
+const redisClient = redis.createClient({
   host: keys.redisHost,
   port: keys.redisPort,
   retry_strategy: () => 1000,
 });
-const sub = redisClinet.duplicate();
+const sub = redisClient.duplicate();
 
 function fib(index) {
   if (index < 2) return 1;
@@ -14,6 +14,6 @@ function fib(index) {
 }
 
 sub.on("message", (channel, message) => {
-  redisClinet.hset("values", message, fib(parseInt(message)));
+  redisClient.hset("values", message, fib(parseInt(message)));
 });
 sub.subscribe("insert");
